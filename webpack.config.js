db54b5389b1541d9d5c350d323f649e0c3726093
IceOutMan <ainewbeginglf@gmail.com>
2023-03-01 22:06:54 +0800

    init: html css js img , loaders

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+var MinCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var getHtmlConfig = function (name) {
+    return {
+        template: __dirname + '/src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        inject: true,
+        hash: true,
+        // 指定要引入的 HTML 的js模块，对应 entry 的chunk
+        chunks: ['common', name] 
+
+    };
+};
+
+var config = {
+    entry: {
+        'login': [__dirname + '/src/page/login/index.js'],
+        'index': [__dirname + '/src/page/index/index.js'],
+    },
+    output: {
+        // /src/page/login/index.js -> js/login.js 
+        path: path.resolve(__dirname, 'dist'),        
+        filename: 'js/[name].js'
+    },
+    plugins :[ 
+        // css单独打包
+        new MinCssExtractPlugin(
+          {filename : 'css/[name].css',}
+        ),
+        // 打包html
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
+    
+      ],
+      externals: {
+        'jquery' : 'window.jQuery'
+      },
+     
+      module: {
+        rules: [
+            { test : /\.css$/, use : [MinCssExtractPlugin.loader,'css-loader'] },
+            { test : /\.(gif|png|jpg|jpeg|woff|svg|eotttf)\??.*$/, use : {loader:'url-loader', options:{limit:10, name: '[name].[ext]',outputPath:'resource/',publicPath:'../resource/'}}},        ]
+      }
+};
+
+module.exports = config;
